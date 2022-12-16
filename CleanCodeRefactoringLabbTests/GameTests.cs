abdc4d80cf43c9890e4f464c5fd5e16b1af2c171
Lars Norman <90247149+Larsn280@users.noreply.github.com>
2022-12-16 12:52:36 +0100

    AddedTest

---
+++ CleanCodeRefactoringLabbTests/GameTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CleanCodeRefactoringLabb.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        [TestMethod()]
+        public void WelcomeTest()
+        {
+            Game game = new Game();
+            Assert.AreEqual("Enter your user name:\n", game.Welcome());
+        }
+    }
+}
